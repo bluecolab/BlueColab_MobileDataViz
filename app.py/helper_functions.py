bbcef7e858e4196a4eaee5293c168d56353c46a4
Kenji <70079754+OkayKenji@users.noreply.github.com>
2024-03-13 02:30:54 -0400

    improve documentation

--- app.py/helper_functions.py
+++ app.py/helper_functions.py
@@ -4,17 +4,16 @@ from pandas import DataFrame
 import calendar
 from WaterData import WaterData
 
-
 def get_years(**kwargs):
     """Returns a list containing years from current year to 2021. For the dropdowns.
 
-    Keyword Arguments:
+    ### Keyword Arguments:
         show_na (bool, optional): If True, appends "NA" to the list. Defaults to False.
 
-    Returns:
+    ### Returns:
         list: A list containing years from current year to 2021. If show_na is True, "NA" is appended to the list.
 
-    Example:
+    ### Example:
         >>> get_years()
         [current year, ..., 2023, 2022, 2021]  # List of years from the current year back to 2021
         >>> get_years(show_na=True)
@@ -43,7 +42,7 @@ def fetch_data(location: str | None = "Choate Pond",
 
     """Fetches data from APIs.
 
-    Args:
+    ### Args:
         location (str): Location to get water data from. Default is "Choate Pond".
         start_year (int): Year to start getting data from.
         start_month (int): Month to start getting data from.
@@ -51,10 +50,10 @@ def fetch_data(location: str | None = "Choate Pond",
         end_year (int): Year to end getting data from.
         end_month (int): Month to end getting data from.
         end_day (int): Day to end getting from. 
-    Returns:
+    ### Returns:
         DataFrame: Data fetched from the specified location for the given dates.
 
-    Raises:
+    ### Raises:
         ValueError: If any of the date parameters are know, will raise a value error.
     """
     if any(param is None for param in [start_year, start_month, start_day, end_year, end_month, end_day]):
@@ -81,15 +80,15 @@ def fetch_data_caller(location: str | None = "Choate Pond",
                       month: int | None = None) -> DataFrame:
     """Helper function to get monthly water data given location, month, and year as a DataFrame
 
-    Args:
+    ### Args:
         location (str): Location to get water data from. Default is "Choate Pond".
         year (int): Year to get water data from.
         month (int): Month to get water data from as a month name.
 
-    Returns:
+    ### Returns:
         DataFrame: Data fetched from the specified location for the given month and year.
 
-    Raises:
+    ### Raises:
         ValueError: If `year` or `month` is `None`.
     """
     
@@ -104,4 +103,3 @@ def fetch_data_caller(location: str | None = "Choate Pond",
     end_day = calendar.monthrange(int(year), datetime.datetime.strptime(month, "%B").month)[1] # find the last day of the month in given year
 
     return fetch_data(location,start_year,start_month,start_day,end_year,end_month,end_day)
-
