7fe5a9b631efe0621dc12d7ce21d6466e7e9191a
Kenji <70079754+OkayKenji@users.noreply.github.com>
2024-03-13 05:53:20 -0400

    commented starter code

--- app.py/app.py
+++ app.py/app.py
@@ -4,9 +4,21 @@ from helper_functions import get_years, fetch_data_caller
 import plotly.express as px
 import calendar, datetime
 
-# Get the name of the last month
+# Noting so we don't forget missing features
+# TODO: (later) Add back WQI
+# TODO: (later) Add back text that summerized the montly data
+# TODO: (later) Add url parameters
+# TODO: (overall) Improve the UI 
+# TODO: (later) Convert everything to the right units
+# TODO: (later) Cleaning data up, removing outliers. Python makes this easier...!
+
 @render.text
-def slider_val():
+def output_text_verbatim():
+    """Creates the title above the graph
+
+    Returns:
+        str: Title to be set
+    """
     parameter = input.parameter()
     location_1 = input.location_1()
     location_2 = input.location_2()
@@ -22,7 +34,12 @@ def slider_val():
 
 @render_plotly
 def plot1():
-    # getting parameters from dropdowns
+    """Generates the graph
+
+    Returns:
+        px: Graph to display
+    """
+    # getting parameters from the dropdowns
     parameter = input.parameter()
     location_1 = input.location_1()
     location_2 = input.location_2()
@@ -32,14 +49,48 @@ def plot1():
     year_2 = input.year_2()
 
     df = fetch_data_caller(location_1,year_1,month_1).data
+    # ^ This DataFrame looks like the one in health_dashboard GitHub. Where each parameter was in its own columns
+
+    # TODO: (Bonus): Convert the temperures to farienheight 
 
     if location_1 == "Choate Pond":
         full_to_short_names  = {'Conductivity': 'Cond', 'Dissolved Oxygen': 'DOpct', 'Salinity': 'Sal','Temperature': 'Temp','Turbidity': 'Turb'}
-        p = px.line(df, x='timestamp', y=full_to_short_names[parameter])
-        p.update_layout(height=200, xaxis_title=None)
-    # TODO: Add support for other water location
+
+        df_param_only = df[["timestamp",full_to_short_names[parameter]]]
+        # TODO: (before Friday?): We have this DataFrame df_param_only. It has ALL the data of the whole month for the selected parameter. That's like 2976 rows per month?
+        # Tasks: 
+        #   - Create a new df, so it only 31 rows, one row for each day
+        #   - Each row should have the (1) min (2) max (3) average of each day of the month
+        #     so the df would look kinda like this. The names of the cols might be different 
+        # 
+        #     timestamp   min_Cond max_Cond avg_Cond
+        # 0   01-01-2024  22        76       33
+        # 1   01-02-2024  54        76       33
+        # 2   01-03-2024  22        76       54
+        # 3   01-04-2024  22        32       11
+        # 3   01-05-2024  34        76       44
+
+        df_param_only = df_param_only # your code here idk 
         
-    # TODO: Add support for second location/time to compare
+        # TODO: (before Friday?): Now that we have the df with min/max/avg working, please make the graph.
+        # Tasks:
+        #   - Forget, the code we have already. What we want to do is add a ribbon to our graph. Looking up ribbon plotly or something 
+        #     may help. Ribbon can be any color that makes sense. 
+        #   - Essentially, the min_Cond would serve as the bottom of the ribbon. max the top of the ribbon.
+        #   - And avg_Cond just a line graph. 
+        
+        # TODO: (Bonus): Look at the min_Cond and max_Cond. Draw a tick mark on the graph at that point if it's out of sensor thresholds. 
+        # Tasks:
+        #   - This I honestly don't know. But you got this 
+
+        p = px.line(df_param_only, x='timestamp', y=full_to_short_names[parameter])
+        p.update_layout(height=200, xaxis_title=None)
+    
+    
+    
+    # TODO: (soon) Add support for other water location
+        
+    # TODO: (soon) Add support for second location/time to compare
         
     return p
 
