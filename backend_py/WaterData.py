2cbce42220c4fe105d9b54dffed13f4318ec56d4
Kenji <okurakeng@gmail.com>
2025-02-11 21:00:14 -0500

    remove backend

--- backend_py/WaterData.py
+++
@@ -1,7 +0,0 @@
-class WaterData:
-  """This is a simple Python class so the fetch_data function 
-     returns both data (data frame) and WQI (if applicable)
-  """
-  def __init__(self, data, wqi):
-    self.data = data
-    self.wqi = wqi
