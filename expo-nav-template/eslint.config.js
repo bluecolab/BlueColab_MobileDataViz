28647e286df78e21985c2e1383d89a95218145cf
Kenji Okura <okurakeng@gmail.com>
2025-06-09 20:13:30 -0400

    Re-enable eslint

---
+++ expo-nav-template/eslint.config.js
@@ -0,0 +1,54 @@
+import pluginImport from 'eslint-plugin-import';
+import path from 'path';
+import tsParser from '@typescript-eslint/parser';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import tseslint from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['app/**/*.{js,ts,jsx,tsx}', 'components/**/*.{js,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      import: pluginImport,
+      '@typescript-eslint': tseslint,
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+    },
+    settings: {
+      react: { version: 'detect' },
+      'import/resolver': {
+        typescript: {
+          project: path.resolve('./tsconfig.json'),
+        },
+      },
+    },
+    rules: {
+      // Imports
+      'import/no-unresolved': 'error',
+      'import/namespace': 'error',
+      'import/no-duplicates': 'warn',
+
+      // React
+      'react/jsx-key': 'warn',
+      'react/no-unstable-nested-components': 'warn',
+
+      // React Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // TypeScript rules
+      '@typescript-eslint/no-unused-vars': 'warn',
+    //   '@typescript-eslint/no-floating-promises': 'error',
+    //   '@typescript-eslint/explicit-function-return-type': 'warn',
+    },
+  },
+];
