e8fa200d511fd5ced5cf369afa7a6a5e9f72b652
Kenji Okura <okurakeng@gmail.com>
2025-08-05 07:53:00 -0400

    AWM-47 - Added TypeScript checking

--- aquawatch_mobile_app/prettier.config.js
+++ aquawatch_mobile_app/prettier.config.js
@@ -4,6 +4,7 @@ module.exports = {
     singleQuote: true,
     bracketSameLine: true,
     trailingComma: 'es5',
+    semi: true,
     endOfLine: 'auto',
     plugins: [require.resolve('prettier-plugin-tailwindcss')],
     tailwindAttributes: ['className'],
