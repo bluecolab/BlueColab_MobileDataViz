845eaef577bc9018c3e548aa52f5b8add2021f20
Kenji Okura <okurakeng@gmail.com>
2025-08-25 23:16:34 -0400

    Fix plugin order

--- aquawatch_mobile_app/babel.config.js
+++ aquawatch_mobile_app/babel.config.js
@@ -1,10 +1,7 @@
 module.exports = function (api) {
     api.cache(true);
-    const plugins = ['react-native-reanimated/plugin'];
-
     return {
-        presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
-        plugins,
+        presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
+        plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
     };
 };
