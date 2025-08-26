b33ae0c628abb3c6dc8578fc72235930e6e33a18
Kenji Okura <okurakeng@gmail.com>
2025-08-25 23:25:38 -0400

    Revert "Fix plugin order"
    This reverts commit 845eaef577bc9018c3e548aa52f5b8add2021f20.

--- aquawatch_mobile_app/babel.config.js
+++ aquawatch_mobile_app/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
     api.cache(true);
+    const plugins = ['react-native-reanimated/plugin'];
+
     return {
-        presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
-        plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
+        presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+        plugins,
     };
 };
