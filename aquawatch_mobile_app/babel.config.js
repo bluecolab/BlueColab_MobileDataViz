3191096a4c1c443ac8f7b1855804a9d1a397d213
Kenji Okura <okurakeng@gmail.com>
2025-09-01 18:25:28 -0400

    Babel fix

--- aquawatch_mobile_app/babel.config.js
+++ aquawatch_mobile_app/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
     api.cache(true);
-    const plugins = ['react-native-reanimated/plugin'];
+    const plugins = ['react-native-worklets/plugin'];
 
     return {
         presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
