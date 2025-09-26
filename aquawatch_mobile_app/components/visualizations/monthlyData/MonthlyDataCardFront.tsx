66c25de9d3d9da38b9435f0869fcc90fe5aaacf2
Kenji Okura <okurakeng@gmail.com>
2025-09-25 23:52:16 -0400

    Make monthly data flip supported as well

--- aquawatch_mobile_app/components/visualizations/monthlyData/MonthlyDataCardFront.tsx
+++ aquawatch_mobile_app/components/visualizations/monthlyData/MonthlyDataCardFront.tsx
@@ -1,3 +1,4 @@
+import { FontAwesome } from '@expo/vector-icons';
 import { View, Text } from 'react-native';
 
 import { useColorScheme } from '@/contexts/ColorSchemeContext';
@@ -13,6 +14,7 @@ interface MonthlyDataCardFrontProp {
     dailySummary: DailySummaryType[];
     error: ErrorType | undefined;
     month: string;
+    title: string;
 }
 
 export function MonthlyDataCardFront({
@@ -20,6 +22,7 @@ export function MonthlyDataCardFront({
     dailySummary,
     error,
     month,
+    title,
 }: MonthlyDataCardFrontProp) {
     const { isDark, loading: fontLoading, font } = useColorScheme();
 
@@ -29,27 +32,42 @@ export function MonthlyDataCardFront({
 
     if (loading) {
         return (
-            <EmptyGraph
-                error={{
-                    message: 'Loading...',
-                    code: 0,
-                }}
-            />
+            <View className="z-10 h-[340] w-[95%] self-center">
+                <EmptyGraph
+                    error={{
+                        message: 'Loading...',
+                        code: 0,
+                    }}
+                />
+            </View>
         );
     } else if (error) {
         return <EmptyGraph error={error} />;
     } else if (dailySummary.length === 0) {
         return (
-            <EmptyGraph
-                error={{
-                    message: 'Error: No data available',
-                }}
-            />
+            <View className="z-10 h-[340] w-[95%] self-center">
+                <EmptyGraph
+                    error={{
+                        message: 'Error: No data available',
+                    }}
+                />
+            </View>
         );
     }
 
     return (
-        <View className="h-full rounded-3xl bg-white px-2 dark:bg-gray-700">
+        <View className="h-[340] rounded-3xl bg-white px-2 dark:bg-gray-700">
+            <View className="w-full self-center">
+                <Text className="rounded-3xl bg-white p-1 text-center text-2xl font-bold dark:bg-gray-700 dark:text-white">
+                    {title}
+                </Text>
+                <FontAwesome
+                    className="absolute right-0 top-2"
+                    name="info-circle"
+                    size={32}
+                    color={isDark ? 'white' : 'grey'}
+                />
+            </View>
             <Text
                 className="absolute bottom-1 left-1/2 -translate-x-1/2 text-center text-black dark:text-white"
                 key="month-label">
