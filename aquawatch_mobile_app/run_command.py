09d0d9d8b07e11f83bfebc3dce7297c65ccc41b1
Kenji Okura <okurakeng@gmail.com>
2025-10-17 00:26:30 -0400

    Delete this

---
+++ aquawatch_mobile_app/run_command.py
@@ -0,0 +1,23 @@
+import http.server
+import os
+import socketserver
+
+PORT = 8080
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == '/event':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Starting the move to production!')
+            print("Button event received from client!")
+            os.system("npx expo start -w")
+        else:
+            self.send_error(404)
+
+Handler = MyHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving HTTP on 0.0.0.0:{PORT}")
+    httpd.serve_forever()
