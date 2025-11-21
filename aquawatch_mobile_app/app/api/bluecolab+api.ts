7ea746fbfc17b088dd2d33010bbfe624091047e9
Kenji Okura <okurakeng@gmail.com>
2025-11-21 09:11:02 -0500

    Send link

--- aquawatch_mobile_app/app/api/bluecolab+api.ts
+++ aquawatch_mobile_app/app/api/bluecolab+api.ts
@@ -1,7 +1,5 @@
 export async function POST(request: Request) {
     const { request: url } = await request.json();
-    console.log(url);
     const response = await fetch(url);
-    console.log('Response status:', response.status);
     return Response.json(await response.json());
 }
---
+++ aquawatch_mobile_app/app/api/waterreports+api.ts
@@ -0,0 +1,4 @@
+export async function GET() {
+    const api_address = "http://10.86.80.189";
+    return Response.json({ api_address });
+}
