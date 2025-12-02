760d6d7e2302492a5f13dee83c722a52dd522407
Kenji Okura <okurakeng@gmail.com>
2025-12-01 23:22:15 -0500

    Fix layout issues

--- aquawatch_mobile_app/components/customCards/HomePageCardMini.tsx
+++ aquawatch_mobile_app/components/customCards/HomePageCardMini.tsx
@@ -13,14 +13,14 @@ export function HomepageCard({
     path: string;
 }) {
     return (
-        <View className="mx-4 my-2 flex-row items-center rounded-xl bg-white p-4">
-            <Pressable onPress={() => router.push(path as any)}>
-                <Image source={image} className="mr-4 h-24 w-24 rounded-lg" resizeMode="cover" />
-                <View className="flex-1">
-                    <Text className="mb-1 text-lg font-bold text-[#263A75]">{title}</Text>
-                    <Text className="text-[#263A75]">{description}</Text>
-                </View>
-            </Pressable>
-        </View>
+        <Pressable
+            onPress={() => router.push(path as any)}
+            className="mx-4 my-2 flex-row items-center rounded-xl bg-white p-4">
+            <Image source={image} className="mr-4 h-24 w-24 rounded-lg" resizeMode="cover" />
+            <View className="flex-1">
+                <Text className="mb-1 text-lg font-bold text-[#263A75]">{title}</Text>
+                <Text className="text-[#263A75]">{description}</Text>
+            </View>
+        </Pressable>
     );
 }
