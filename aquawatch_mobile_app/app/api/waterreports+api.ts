4281c75a5761c544b3f5e1cc36bfd4d8d72770d7
Kenji Okura <okurakeng@gmail.com>
2025-12-05 14:41:02 -0500

    new ip

--- aquawatch_mobile_app/app/api/waterreports+api.ts
+++ aquawatch_mobile_app/app/api/waterreports+api.ts
@@ -1,4 +1,4 @@
 export async function GET() {
-    const api_address = 'http://10.86.80.189:8000';
+    const api_address = 'http://10.86.80.188:8000';
     return Response.json({ api_address });
 }
